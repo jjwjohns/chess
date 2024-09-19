6780c015c14d7806a69bb0383f32f10869490012
jjwjohns <jordanwjohns@gmail.com>
2024-09-18 21:55:09 -0600

    moved functions over to a package

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,4 +1,5 @@
 package chess;
+import chess.move_calculator.moves_calculator;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -77,59 +78,59 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
 
-    private Collection<ChessMove> Bishop_move_calculator(ChessBoard board, ChessPosition position){
-        Collection<ChessMove> Moves = new ArrayList<>();
-        int row = position.getRow();
-        int col = position.getColumn();
-
-        int[][] possible_directions = {{1,1},{1,-1},{-1,1},{-1,-1}};
-
-        for (int[] direction : possible_directions) {
-            if (row + direction[0] >=1 && row+direction[0] <=8 && col+direction[1] >=1 && col+direction[1] <=8){
-                for (int newR = row + direction[0], newC = col+direction[1]; newR <= 8 && newC <= 8 && newR >= 1 && newC >= 1; newR += direction[0], newC += direction[1]) {
-                    ChessPosition newPosition = new ChessPosition(newR, newC);
-                    if (board.getPiece(newPosition) == null){
-                        Moves.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(newR, newC), null));
-                    }
-                    else if (board.getPiece(newPosition).getTeamColor() != this.pieceColor){
-                        Moves.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(newR, newC), null));
-                        break;
-                    }
-                    else {
-                        break;
-                    }
-                }
-            }
-        }
-        return Moves;
-    }
-
-    private Collection<ChessMove> Knight_move_calculator(ChessBoard board, ChessPosition position){
-        Collection<ChessMove> Moves = new ArrayList<>();
-        int row = position.getRow();
-        int col = position.getColumn();
-
-        int[][] possible_moves = {{1,2},{1,-2},{-1,2},{-1,-2},{2,1},{-2,1},{-2,-1},{2,-1}};
-        for (int[] move : possible_moves) {
-            int newR = row + move[0];
-            int newC = col + move[1];
-
-            if (newR <= 8 && newC <= 8 && newR >= 1 && newC >= 1) {
-                ChessPosition newPosition = new ChessPosition(newR, newC);
-                if (board.getPiece(newPosition) == null || board.getPiece(newPosition).getTeamColor() != this.pieceColor) {
-                    Moves.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(newR, newC), null));
-                }
-            }
-        }
-        return Moves;
-    }
+//    private Collection<ChessMove> Bishop_move_calculator(ChessBoard board, ChessPosition position){
+//        Collection<ChessMove> Moves = new ArrayList<>();
+//        int row = position.getRow();
+//        int col = position.getColumn();
+//
+//        int[][] possible_directions = {{1,1},{1,-1},{-1,1},{-1,-1}};
+//
+//        for (int[] direction : possible_directions) {
+//            if (row + direction[0] >=1 && row+direction[0] <=8 && col+direction[1] >=1 && col+direction[1] <=8){
+//                for (int newR = row + direction[0], newC = col+direction[1]; newR <= 8 && newC <= 8 && newR >= 1 && newC >= 1; newR += direction[0], newC += direction[1]) {
+//                    ChessPosition newPosition = new ChessPosition(newR, newC);
+//                    if (board.getPiece(newPosition) == null){
+//                        Moves.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(newR, newC), null));
+//                    }
+//                    else if (board.getPiece(newPosition).getTeamColor() != this.pieceColor){
+//                        Moves.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(newR, newC), null));
+//                        break;
+//                    }
+//                    else {
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//        return Moves;
+//    }
+//
+//    private Collection<ChessMove> Knight_move_calculator(final ChessGame.TeamColor pieceColor, ChessBoard board, ChessPosition position){
+//        Collection<ChessMove> Moves = new ArrayList<>();
+//        int row = position.getRow();
+//        int col = position.getColumn();
+//
+//        int[][] possible_moves = {{1,2},{1,-2},{-1,2},{-1,-2},{2,1},{-2,1},{-2,-1},{2,-1}};
+//        for (int[] move : possible_moves) {
+//            int newR = row + move[0];
+//            int newC = col + move[1];
+//
+//            if (newR <= 8 && newC <= 8 && newR >= 1 && newC >= 1) {
+//                ChessPosition newPosition = new ChessPosition(newR, newC);
+//                if (board.getPiece(newPosition) == null || board.getPiece(newPosition).getTeamColor() != this.pieceColor) {
+//                    Moves.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(newR, newC), null));
+//                }
+//            }
+//        }
+//        return Moves;
+//    }
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         if (this.type == PieceType.BISHOP) {
-            return Bishop_move_calculator(board, myPosition);
+            return chess.move_calculator.moves_calculator.Bishop_move_calculator(this, board, myPosition);
         }
         else if (this.type == PieceType.KNIGHT) {
-            return Knight_move_calculator(board, myPosition);
+            return chess.move_calculator.moves_calculator.Knight_move_calculator(this, board, myPosition);
         }
         else return null;
     }
---
+++ shared/src/main/java/chess/move_calculator/moves_calculator.java
@@ -0,0 +1,58 @@
+package chess.move_calculator;
+
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+import chess.ChessPiece;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class moves_calculator {
+    public static Collection<ChessMove> Bishop_move_calculator(ChessPiece piece, ChessBoard board, ChessPosition position){
+        Collection<ChessMove> Moves = new ArrayList<>();
+        int row = position.getRow();
+        int col = position.getColumn();
+
+        int[][] possible_directions = {{1,1},{1,-1},{-1,1},{-1,-1}};
+
+        for (int[] direction : possible_directions) {
+            if (row + direction[0] >=1 && row+direction[0] <=8 && col+direction[1] >=1 && col+direction[1] <=8){
+                for (int newR = row + direction[0], newC = col+direction[1]; newR <= 8 && newC <= 8 && newR >= 1 && newC >= 1; newR += direction[0], newC += direction[1]) {
+                    ChessPosition newPosition = new ChessPosition(newR, newC);
+                    if (board.getPiece(newPosition) == null){
+                        Moves.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(newR, newC), null));
+                    }
+                    else if (board.getPiece(newPosition).getTeamColor() != piece.getTeamColor()){
+                        Moves.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(newR, newC), null));
+                        break;
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+        }
+        return Moves;
+    }
+
+    public static Collection<ChessMove> Knight_move_calculator(ChessPiece piece, ChessBoard board, ChessPosition position){
+        Collection<ChessMove> Moves = new ArrayList<>();
+        int row = position.getRow();
+        int col = position.getColumn();
+
+        int[][] possible_moves = {{1,2},{1,-2},{-1,2},{-1,-2},{2,1},{-2,1},{-2,-1},{2,-1}};
+        for (int[] move : possible_moves) {
+            int newR = row + move[0];
+            int newC = col + move[1];
+
+            if (newR <= 8 && newC <= 8 && newR >= 1 && newC >= 1) {
+                ChessPosition newPosition = new ChessPosition(newR, newC);
+                if (board.getPiece(newPosition) == null || board.getPiece(newPosition).getTeamColor() != piece.getTeamColor()) {
+                    Moves.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(newR, newC), null));
+                }
+            }
+        }
+        return Moves;
+    }
+}
