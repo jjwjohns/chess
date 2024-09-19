dad1f14c1b2577185d2cc8910d8afa095580a9b3
jjwjohns <jordanwjohns@gmail.com>
2024-09-18 22:06:46 -0600

    passed king move test

--- shared/src/main/java/chess/move_calculator/moves_calculator.java
+++ shared/src/main/java/chess/move_calculator/moves_calculator.java
@@ -55,4 +55,22 @@ public class moves_calculator {
         }
         return Moves;
     }
+    public static Collection<ChessMove> King_move_calculator(ChessPiece piece, ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> Moves = new ArrayList<>();
+        int row = position.getRow();
+        int col = position.getColumn();
+        int[][] possible_moves = {{1,1},{1,-1},{1,0},{0,1},{0,-1},{-1,1},{-1,0},{-1,-1}};
+
+        for (int[] move : possible_moves) {
+            int newR = row + move[0];
+            int newC = col + move[1];
+            if (newR <= 8 && newC <= 8 && newR >= 1 && newC >= 1) {
+                ChessPosition newPosition = new ChessPosition(newR, newC);
+                if (board.getPiece(newPosition) == null || board.getPiece(newPosition).getTeamColor() != piece.getTeamColor()) {
+                    Moves.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(newR, newC), null));
+                }
+            }
+        }
+        return Moves;
+    }
 }
