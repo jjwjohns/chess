6a74b754112415905366410b799fbdec188a04f3
jjwjohns <jordanwjohns@gmail.com>
2024-12-05 15:24:27 -0700

    invalid move test passed

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,7 +1,9 @@
 package server.websocket;
 
+import chess.ChessBoard;
 import chess.ChessGame;
 import chess.ChessMove;
+import chess.ChessPiece;
 import com.google.gson.Gson;
 import dataaccess.DataAccessException;
 import model.Authtoken;
@@ -13,6 +15,7 @@ import websocket.commands.UserGameCommand;
 import websocket.messages.ServerMessage;
 
 import java.io.IOException;
+import java.util.Collection;
 
 
 @WebSocket
@@ -25,73 +28,83 @@ public class WebSocketHandler {
 
   @OnWebSocketMessage
     public void onMessage(Session session, String message) throws Exception {
-        this.session = session;
+      this.session = session;
 
-       UserGameCommand action = new Gson().fromJson(message, UserGameCommand.class);
-        switch (action.getCommandType()) {
-            case CONNECT -> connect(action);
-            case MAKE_MOVE -> makeMove(action);
-            case LEAVE -> leave();
-            case RESIGN -> resign();
-        }
+     UserGameCommand action = new Gson().fromJson(message, UserGameCommand.class);
+      switch (action.getCommandType()) {
+          case CONNECT -> connect(action);
+          case MAKE_MOVE -> makeMove(action);
+          case LEAVE -> leave();
+          case RESIGN -> resign();
+      }
     }
 
 
     private void connect(UserGameCommand action) throws Exception {
-
-          String auth = action.getAuthToken();
-          Integer gameID = action.getGameID();
-          Authtoken token = Server.dataAccess.getAuth(auth);
-          if (token == null){
-            ServerMessage notification = new ServerMessage(ServerMessage.ServerMessageType.ERROR, "Error: Authtoken is invalid");
-            session.getRemote().sendString(new Gson().toJson(notification));
-            return;
-          }
-
-        String user = token.username();
-
-        connections.add(user, gameID, this.session);
-        ChessGame game;
-        try {
-          game = Server.dataAccess.getGame(gameID).game();
-        } catch (Exception e){
-          ServerMessage notification = new ServerMessage(ServerMessage.ServerMessageType.ERROR, "Error: Game ID is invalid");
-          session.getRemote().sendString(new Gson().toJson(notification));
-          return;
-        }
-
-
-        System.out.println(action.getCommandType());
-
-        var notification = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME, game);
+      String auth = action.getAuthToken();
+      Integer gameID = action.getGameID();
+      Authtoken token = Server.dataAccess.getAuth(auth);
+      if (token == null){
+        ServerMessage notification = new ServerMessage(ServerMessage.ServerMessageType.ERROR, "Error: Authtoken is invalid");
         session.getRemote().sendString(new Gson().toJson(notification));
+        return;
+      }
 
-        String message = "player " + user + " has joined game " + gameID;
+      String user = token.username();
 
-        notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
-        connections.broadcast(user, gameID, notification);
+      connections.add(user, gameID, this.session);
+      ChessGame game;
+      try {
+        game = Server.dataAccess.getGame(gameID).game();
+      } catch (Exception e){
+        ServerMessage notification = new ServerMessage(ServerMessage.ServerMessageType.ERROR, "Error: Game ID is invalid");
+        session.getRemote().sendString(new Gson().toJson(notification));
+        return;
+      }
+
+
+      System.out.println(action.getCommandType());
+
+      var notification = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME, game);
+      session.getRemote().sendString(new Gson().toJson(notification));
+
+      String message = "player " + user + " has joined game " + gameID;
+
+      notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
+      connections.broadcast(user, gameID, notification);
     }
 
     private void makeMove(UserGameCommand action) throws DataAccessException, IOException {
-        ChessMove move = action.getMove();
-        String auth = action.getAuthToken();
-        Integer gameID = action.getGameID();
-        Authtoken token = Server.dataAccess.getAuth(auth);
-        if (token == null){
-          ServerMessage notification = new ServerMessage(ServerMessage.ServerMessageType.ERROR, "Error: Authtoken is invalid");
-          session.getRemote().sendString(new Gson().toJson(notification));
-          return;
-        }
+      String auth = action.getAuthToken();
+      Authtoken token = Server.dataAccess.getAuth(auth);
+      if (token == null){
+        ServerMessage notification = new ServerMessage(ServerMessage.ServerMessageType.ERROR, "Error: Authtoken is invalid");
+        session.getRemote().sendString(new Gson().toJson(notification));
+        return;
+      }
+
+      Integer gameID = action.getGameID();
+      ChessMove move = action.getMove();
+      ChessGame game = Server.dataAccess.getGame(gameID).game();
+      ChessBoard board = game.getBoard();
+      ChessPiece piece = board.getPiece(move.getStartPosition());
+      Collection<ChessMove> moves = piece.pieceMoves(board, move.getStartPosition());
+
+      if (!moves.contains(move)){
+        ServerMessage notification = new ServerMessage(ServerMessage.ServerMessageType.ERROR, "Error: Move is invalid");
+        session.getRemote().sendString(new Gson().toJson(notification));
+        return;
+      }
 
     }
 
     private void leave(){
-        connections.remove("user");
-        System.out.println("leave not implemented");
+      connections.remove("user");
+      System.out.println("leave not implemented");
     }
 
     private void resign(){
-        System.out.println("resign not implemented");
+      System.out.println("resign not implemented");
     }
 }
 
